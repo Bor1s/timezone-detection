bf21ae277f0d9cbe65e8847fa5c9420aa00f53fd
Boris Babusenko <boris@cleverua.com>
2012-08-09 15:35:07 +0300

    Improves caching for timezone lookup. Bumps version to 0.0.4

--- lib/timezone_detection/application_controller_extension.rb
+++ lib/timezone_detection/application_controller_extension.rb
@@ -1,9 +1,12 @@
 module TimezoneDetection
 	module ClassMethods
 		def set_timezone
-			ip_info = IpInfoDB.new
-			offset = ip_info.get_timezone_offset(request.remote_ip)
-			ip_timezone = IpTimezone.create(ip: request.remote_ip, utc_offset: offset)
+			ip_timezone = IpTimezone.find_ip(request.remote_ip).first
+			unless ip_timezone.present?
+				ip_info = IpInfoDB.new
+				offset = ip_info.get_timezone_offset(request.remote_ip)
+				ip_timezone = IpTimezone.create(ip: request.remote_ip, utc_offset: offset)
+			end
 			Time.zone = ActiveSupport::TimeZone[ip_timezone.utc_offset]
 		end
 	end
